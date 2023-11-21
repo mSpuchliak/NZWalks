5132b5ae9baa10b8aefd1c76f7b566944b985932
Martin Spuchliak <martinspuchliak@gmail.com>
2023-11-21 19:19:54 +0100

    Added Images to the WebAPI

---
+++ NZWalks.API/Controllers/ImagesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImageRepository imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDTO request)
+        {
+            ValidateFileUpload(request);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var imageDomainModel = new Image
+            {
+                File = request.File,
+                FileExtension = Path.GetExtension(request.File.FileName),
+                FileSizeInBytes = request.File.Length,
+                FileName = request.FileName,
+                FileDescription = request.FileDescription
+            };
+            
+            await imageRepository.Upload(imageDomainModel);
+
+            return Ok(imageDomainModel);
+        }
+
+        private void ValidateFileUpload(ImageUploadRequestDTO request)
+        {
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+
+            if (!allowedExtensions.Contains(Path.GetExtension(request.File.FileName)))
+            {
+                ModelState.AddModelError("file", "Unsuported file extension");
+            }
+
+            // Cannot be bigger than 10MB
+            if(request.File.Length > 10485760)
+            {
+                ModelState.AddModelError("file", "File size is bigger than 10MB, upload a smaller file");
+            }
+        }
+    }
+}
--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -7,12 +7,15 @@ using NZWalks.API.Mappings;
 using NZWalks.API.Repositories;
 using System.Text;
 using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpContextAccessor();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
@@ -52,6 +55,7 @@ builder.Services.AddDbContext<NZWalksAuthDbContext>(options => options.UseSqlSer
 builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();
 builder.Services.AddScoped<IWalkRepository, SQLWalkRepository>();
 builder.Services.AddScoped<ITokenRepository, TokenRepository>();
+builder.Services.AddScoped<IImageRepository, LocalImageRepository>();
 
 builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
 
@@ -98,6 +102,12 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 
 app.UseAuthorization();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Images")),
+    RequestPath = "/Images"
+});
 
 app.MapControllers();
 
